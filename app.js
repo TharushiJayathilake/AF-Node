9083b817934c742d74e261d65f3d8e49fe8f98ab
TharushiJayathilake <tjayathilaka21@gmail.com>
2026-02-22 11:52:46 +0530

    creating  web server

--- app.js
+++ app.js
@@ -11,11 +11,20 @@ console.log("Hello Node Lab!");
 
 
 //write file
-const fs = require('fs');
 
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
-});
+// const fs = require('fs');
 
+// fs.writeFile('file.txt', 'Hello World!', function (err) {
+//   if (err) throw err;
+//   console.log('File saved!');
+// });
+
+//web server
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
 
